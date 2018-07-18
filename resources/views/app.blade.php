0d1f972718fa8f6a2229746042e8927bbb1893b3
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-18 14:22:20 +0200

    adjusted home route

---
+++ resources/views/app.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>{{ env('APP_NAME') }}</title>
+
+    {!! Html::style('css/app.css') !!}
+
+    {!! Html::script('js/app.js') !!}
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
