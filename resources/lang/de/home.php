b42d0f1cccc24d0e118e9dbe493a2cb815ab77b4
Andreas Wiedel <andreas.wiedel@smartservice.de>
2018-07-18 14:30:24 +0200

    adjusted home route

---
+++ resources/lang/de/home.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'index' => [
+        'description' => 'Wir treffen uns jeden 1. Samstag eines Monats. Treffpunk ist der Bayreuther Hauptbahnhof gegen 14:00 Uhr.',
+        'events' => 'Eventreihe',
+    ],
+];
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,9 @@
 @extends('app')
 
 @section('content')
+    <div class="container">
+        <p>@lang('home.index.description')</p>
 
+        <a class="button" href="http://bit.ly/manga-treff-bayreuth">@lang('home.index.events')</a>
+    </div>
 @endsection
