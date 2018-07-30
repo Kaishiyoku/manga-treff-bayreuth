00ce0492134dc028c6d313e521a7f2090df46ce8
Kaishiyoku <dmp@nfze.de>
2018-07-30 21:59:08 +0200

    MTB-9 showing Anime drawing on landing page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,19 +1,27 @@
 @extends('app')
 
 @section('content')
-    <p class="lead">@lang('home.index.description')</p>
+    <div class="row">
+        <div class="col-md-9 col-sm-8 col-8">
+            <p class="lead">@lang('home.index.description')</p>
 
-    @if ($nextUpcomingEvent)
-        <p class="lead">
-            @lang('home.index.events.next_upcoming_at', ['date' => '<a href="' . $nextUpcomingEvent->getUrl() . '">' . $nextUpcomingEvent->date->format(__('date.short')) . '</a>'])
-        </p>
-    @endif
+            @if ($nextUpcomingEvent)
+                <p class="lead">
+                    @lang('home.index.events.next_upcoming_at', ['date' => '<a href="' . $nextUpcomingEvent->getUrl() . '">' . $nextUpcomingEvent->date->format(__('date.short')) . '</a>'])
+                </p>
+            @endif
 
-    <p>
-        <a class="btn btn-outline-primary" href="{{ env('SHORT_EVENT_BASE_URL') }}">
-            @lang('home.index.events.animexx_series')
-        </a>
-    </p>
+            <p>
+                <a class="btn btn-outline-primary" href="{{ env('SHORT_EVENT_BASE_URL') }}">
+                    @lang('home.index.events.animexx_series')
+                </a>
+            </p>
+        </div>
+
+        <div class="col-md-3 col-sm-4 col-4">
+            <img src="{{ asset('img/drawing.png') }}" class="img-fluid"/>
+        </div>
+    </div>
 
     @if ($nextUpcomingEvent)
         <h1 class="pt-5">@lang('home.index.events.next_upcoming')</h1>
