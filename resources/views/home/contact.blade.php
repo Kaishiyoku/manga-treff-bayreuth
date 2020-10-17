43e36222abec9b508956ffae91437f5e3e191a49
Kaishiyoku <dmp@nfze.de>
2020-10-17 17:21:17 +0200

    adjusted contact page

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -4,52 +4,42 @@
     <h1>{{ __('home.contact.title') }}</h1>
 
     {{ Form::open(['route' => 'home.send_contact_form', 'method' => 'post', 'role' => 'form']) }}
-        <div class="form-group">
-            {{ Form::label('email', __('validation.attributes.email'), ['class' => 'col-md-12 control-label']) }}
+        <div class="mb-4">
+            {{ Form::label('email', __('validation.attributes.email'), ['class' => 'label']) }}
 
-            <div class="col-md-12">
-                {{ Form::email('email', old('email'), ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'required' => true]) }}
+            {{ Form::email('email', old('email'), ['class' => 'input' . ($errors->has('email') ? ' has-error' : ''), 'required' => true, 'placeholder' => __('validation.attributes.email')]) }}
 
-                @if ($errors->has('email'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('email') }}
-                    </div>
-                @endif
-            </div>
+            @if ($errors->has('email'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('email') }}
+                </div>
+            @endif
         </div>
 
-        <div class="form-group">
-            {{ Form::label('fullname', __('validation.attributes.fullname'), ['class' => 'col-md-12 control-label']) }}
+        <div class="mb-4">
+            {{ Form::label('fullname', __('validation.attributes.fullname'), ['class' => 'label']) }}
 
-            <div class="col-md-12">
-                {{ Form::text('fullname', old('fullname'), ['class' => 'form-control' . ($errors->has('fullname') ? ' is-invalid' : ''), 'required' => true]) }}
+            {{ Form::text('fullname', old('fullname'), ['class' => 'input' . ($errors->has('fullname') ? ' has-error' : ''), 'required' => true, 'placeholder' => __('validation.attributes.fullname')]) }}
 
-                @if ($errors->has('fullname'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('fullname') }}
-                    </div>
-                @endif
-            </div>
+            @if ($errors->has('fullname'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('fullname') }}
+                </div>
+            @endif
         </div>
 
-        <div class="form-group">
-            {{ Form::label('content', __('validation.attributes.content'), ['class' => 'col-md-12 control-label']) }}
+        <div class="mb-4">
+            {{ Form::label('content', __('validation.attributes.content'), ['class' => 'label']) }}
 
-            <div class="col-md-12">
-                {{ Form::textarea('content', old('content'), ['class' => 'form-control' . ($errors->has('content') ? ' is-invalid' : ''), 'required' => true]) }}
+            {{ Form::textarea('content', old('content'), ['class' => 'input' . ($errors->has('content') ? ' has-error' : ''), 'required' => true, 'placeholder' => __('validation.attributes.content')]) }}
 
-                @if ($errors->has('content'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('content') }}
-                    </div>
-                @endif
-            </div>
+            @if ($errors->has('content'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('content') }}
+                </div>
+            @endif
         </div>
 
-        <div class="form-group">
-            <div class="col-md-12">
-                {{ Form::button(__('common.send'), ['type' => 'submit', 'class' => 'btn btn-primary']) }}
-            </div>
-        </div>
+        {{ Form::button(__('common.send'), ['type' => 'submit', 'class' => 'btn btn-primary']) }}
     {{ Form::close() }}
 @endsection
