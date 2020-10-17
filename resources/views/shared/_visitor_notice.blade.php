16ee131c8ff77bc9c19f95229564e1254d10af9b
Kaishiyoku <dmp@nfze.de>
2020-10-17 20:44:32 +0200

    minor typography adjustments

--- resources/views/shared/_visitor_notice.blade.php
+++ resources/views/shared/_visitor_notice.blade.php
@@ -1,4 +1,4 @@
-<div class="alert alert-info">
+<div class="alert alert-info prose max-w-none">
     @if ($visitorNotice->title)
         <div class="text-lg font-bold pb-2">{{ $visitorNotice->title }}</div>
     @endif
